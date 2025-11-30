9c2055bfcc0a1b36596b6ff199b4d1203a3a3df1
Abdozakaria222 <abdozakaria222@gmail.com>
2025-12-01 01:47:53 +0200

    Refactor UC_Services.Designer.cs formatting

--- UC_Services.Designer.cs
+++ UC_Services.Designer.cs
@@ -10,7 +10,9 @@ namespace hhh
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
+            {
                 components.Dispose();
+            }
             base.Dispose(disposing);
         }
 
@@ -19,24 +21,23 @@ namespace hhh
             this.btnStop = new System.Windows.Forms.Button();
             this.btnRestore = new System.Windows.Forms.Button();
             this.SuspendLayout();
-
+            //
             // btnStop
+            //
             this.btnStop.Location = new System.Drawing.Point(30, 40);
-            this.btnStop.Name = "btnStop";
             this.btnStop.Size = new System.Drawing.Size(260, 45);
             this.btnStop.Text = "Stop Unneeded Services";
-            this.btnStop.UseVisualStyleBackColor = true;
             this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
-
+            //
             // btnRestore
+            //
             this.btnRestore.Location = new System.Drawing.Point(30, 100);
-            this.btnRestore.Name = "btnRestore";
             this.btnRestore.Size = new System.Drawing.Size(260, 45);
             this.btnRestore.Text = "Restore Default Services";
-            this.btnRestore.UseVisualStyleBackColor = true;
             this.btnRestore.Click += new System.EventHandler(this.btnRestore_Click);
-
+            //
             // UC_Services
+            //
             this.Controls.Add(this.btnStop);
             this.Controls.Add(this.btnRestore);
             this.Name = "UC_Services";
