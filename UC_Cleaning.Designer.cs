a4f9997ac3097bef89412407c40eb66bb3f54f6f
Abdozakaria222 <abdozakaria222@gmail.com>
2025-12-01 01:56:42 +0200

    Refactor button declarations in UC_Cleaning

--- UC_Cleaning.Designer.cs
+++ UC_Cleaning.Designer.cs
@@ -4,14 +4,18 @@ using System.Windows.Forms;
 
 namespace hhh
 {
-    partial class UC_Cleaning
+    public partial class UC_Cleaning : UserControl
     {
         private System.ComponentModel.IContainer components = null;
+        private Button btnCleanTemp;
+        private Button btnCleanCache;
+        private Button btnDNS;
+        private Button btnAll;
 
-        private System.Windows.Forms.Button btnCleanTemp;
-        private System.Windows.Forms.Button btnCleanCache;
-        private System.Windows.Forms.Button btnDNS;
-        private System.Windows.Forms.Button btnAll;
+        public UC_Cleaning()
+        {
+            InitializeComponent();
+        }
 
         protected override void Dispose(bool disposing)
         {
@@ -24,48 +28,43 @@ namespace hhh
 
         private void InitializeComponent()
         {
-            this.btnCleanTemp = new System.Windows.Forms.Button();
-            this.btnCleanCache = new System.Windows.Forms.Button();
-            this.btnDNS = new System.Windows.Forms.Button();
-            this.btnAll = new System.Windows.Forms.Button();
+            this.btnCleanTemp = new Button();
+            this.btnCleanCache = new Button();
+            this.btnDNS = new Button();
+            this.btnAll = new Button();
             this.SuspendLayout();
-            //
+
             // btnCleanTemp
-            //
-            this.btnCleanTemp.Location = new System.Drawing.Point(30, 40);
-            this.btnCleanTemp.Size = new System.Drawing.Size(260, 45);
+            this.btnCleanTemp.Location = new Point(30, 40);
+            this.btnCleanTemp.Size = new Size(260, 45);
             this.btnCleanTemp.Text = "Clean TEMP";
-            this.btnCleanTemp.Click += new System.EventHandler(this.btnCleanTemp_Click);
-            //
+            this.btnCleanTemp.Click += new EventHandler(this.btnCleanTemp_Click);
+
             // btnCleanCache
-            //
-            this.btnCleanCache.Location = new System.Drawing.Point(30, 100);
-            this.btnCleanCache.Size = new System.Drawing.Size(260, 45);
+            this.btnCleanCache.Location = new Point(30, 100);
+            this.btnCleanCache.Size = new Size(260, 45);
             this.btnCleanCache.Text = "Clean Cache";
-            this.btnCleanCache.Click += new System.EventHandler(this.btnCleanCache_Click);
-            //
+            this.btnCleanCache.Click += new EventHandler(this.btnCleanCache_Click);
+
             // btnDNS
-            //
-            this.btnDNS.Location = new System.Drawing.Point(30, 160);
-            this.btnDNS.Size = new System.Drawing.Size(260, 45);
+            this.btnDNS.Location = new Point(30, 160);
+            this.btnDNS.Size = new Size(260, 45);
             this.btnDNS.Text = "Flush DNS";
-            this.btnDNS.Click += new System.EventHandler(this.btnDNS_Click);
-            //
+            this.btnDNS.Click += new EventHandler(this.btnDNS_Click);
+
             // btnAll
-            //
-            this.btnAll.Location = new System.Drawing.Point(30, 220);
-            this.btnAll.Size = new System.Drawing.Size(260, 45);
+            this.btnAll.Location = new Point(30, 220);
+            this.btnAll.Size = new Size(260, 45);
             this.btnAll.Text = "Clean All";
-            this.btnAll.Click += new System.EventHandler(this.btnAll_Click);
-            //
+            this.btnAll.Click += new EventHandler(this.btnAll_Click);
+
             // UC_Cleaning
-            //
             this.Controls.Add(this.btnCleanTemp);
             this.Controls.Add(this.btnCleanCache);
             this.Controls.Add(this.btnDNS);
             this.Controls.Add(this.btnAll);
             this.Name = "UC_Cleaning";
-            this.Size = new System.Drawing.Size(700, 500);
+            this.Size = new Size(700, 500);
             this.ResumeLayout(false);
         }
     }
