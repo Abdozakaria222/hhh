163eced99c8ea738afaac7bb105125956b0b14a7
Abdozakaria222 <abdozakaria222@gmail.com>
2025-12-01 01:48:39 +0200

    Update UC_Cleaning.Designer.cs

--- UC_Cleaning.Designer.cs
+++ UC_Cleaning.Designer.cs
@@ -12,7 +12,9 @@ namespace hhh
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
+            {
                 components.Dispose();
+            }
             base.Dispose(disposing);
         }
 
@@ -23,36 +25,42 @@ namespace hhh
             this.btnDNS = new System.Windows.Forms.Button();
             this.btnAll = new System.Windows.Forms.Button();
             this.SuspendLayout();
-
+            //
             // btnCleanTemp
+            //
             this.btnCleanTemp.Location = new System.Drawing.Point(30, 40);
             this.btnCleanTemp.Size = new System.Drawing.Size(260, 45);
             this.btnCleanTemp.Text = "Clean TEMP";
             this.btnCleanTemp.Click += new System.EventHandler(this.btnCleanTemp_Click);
-
+            //
             // btnCleanCache
+            //
             this.btnCleanCache.Location = new System.Drawing.Point(30, 100);
             this.btnCleanCache.Size = new System.Drawing.Size(260, 45);
             this.btnCleanCache.Text = "Clean Cache";
             this.btnCleanCache.Click += new System.EventHandler(this.btnCleanCache_Click);
-
+            //
             // btnDNS
+            //
             this.btnDNS.Location = new System.Drawing.Point(30, 160);
             this.btnDNS.Size = new System.Drawing.Size(260, 45);
             this.btnDNS.Text = "Flush DNS";
             this.btnDNS.Click += new System.EventHandler(this.btnDNS_Click);
-
+            //
             // btnAll
+            //
             this.btnAll.Location = new System.Drawing.Point(30, 220);
             this.btnAll.Size = new System.Drawing.Size(260, 45);
             this.btnAll.Text = "Clean All";
             this.btnAll.Click += new System.EventHandler(this.btnAll_Click);
-
+            //
             // UC_Cleaning
+            //
             this.Controls.Add(this.btnCleanTemp);
             this.Controls.Add(this.btnCleanCache);
             this.Controls.Add(this.btnDNS);
             this.Controls.Add(this.btnAll);
+            this.Name = "UC_Cleaning";
             this.Size = new System.Drawing.Size(700, 500);
             this.ResumeLayout(false);
         }
