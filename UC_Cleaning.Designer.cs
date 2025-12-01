8c425935f9afe1f84ba75994fd259aafdae380a9
Abdozakaria222 <abdozakaria222@gmail.com>
2025-12-01 02:47:56 +0200

    Refactor UC_Cleaning buttons and localization

--- UC_Cleaning.Designer.cs
+++ UC_Cleaning.Designer.cs
@@ -1,53 +1,71 @@
-using System;
-using System.Drawing;
-using System.Windows.Forms;
-
 namespace hhh
 {
-    public partial class UC_Cleaning : UserControl
+    partial class UC_Cleaning
     {
+        /// <summary> 
+        /// متغيرات مكونات الواجهة
+        /// </summary>
         private System.ComponentModel.IContainer components = null;
-        private Button btnTemp, btnCache, btnDNS, btnAll;
+        private System.Windows.Forms.Button btnCleanTemp;
+        private System.Windows.Forms.Button btnCleanCache;
+        private System.Windows.Forms.Button btnDNS;
+        private System.Windows.Forms.Button btnAll;
 
-        public UC_Cleaning() { InitializeComponent(); }
-
+        /// <summary> 
+        /// تنظيف الموارد
+        /// </summary>
+        /// <param name="disposing">true إذا تم التخلص من الموارد.</param>
         protected override void Dispose(bool disposing)
         {
-            if (disposing && (components != null)) components.Dispose();
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
             base.Dispose(disposing);
         }
 
+        #region كود التهيئة
         private void InitializeComponent()
         {
-            btnTemp  = new Button();
-            btnCache = new Button();
-            btnDNS   = new Button();
-            btnAll   = new Button();
-            SuspendLayout();
+            this.btnCleanTemp = new System.Windows.Forms.Button();
+            this.btnCleanCache = new System.Windows.Forms.Button();
+            this.btnDNS = new System.Windows.Forms.Button();
+            this.btnAll = new System.Windows.Forms.Button();
+            this.SuspendLayout();
 
-            btnTemp.Location = new Point(50, 50);
-            btnTemp.Size = new Size(250, 45);
-            btnTemp.Text = "Clean TEMP";
-            btnTemp.Click += (s, e) => MessageBox.Show("Cleaning TEMP files...");
+            // btnCleanTemp
+            this.btnCleanTemp.Location = new System.Drawing.Point(60, 40);
+            this.btnCleanTemp.Size = new System.Drawing.Size(250, 45);
+            this.btnCleanTemp.Text = "تنظيف ملفات TEMP";
+            this.btnCleanTemp.Click += new System.EventHandler(this.btnCleanTemp_Click);
 
-            btnCache.Location = new Point(50, 110);
-            btnCache.Size = new Size(250, 45);
-            btnCache.Text = "Clean Cache";
-            btnCache.Click += (s, e) => MessageBox.Show("Cleaning Cache...");
+            // btnCleanCache
+            this.btnCleanCache.Location = new System.Drawing.Point(60, 100);
+            this.btnCleanCache.Size = new System.Drawing.Size(250, 45);
+            this.btnCleanCache.Text = "تنظيف ملفات Cache";
+            this.btnCleanCache.Click += new System.EventHandler(this.btnCleanCache_Click);
 
-            btnDNS.Location = new Point(50, 170);
-            btnDNS.Size = new Size(250, 45);
-            btnDNS.Text = "Flush DNS";
-            btnDNS.Click += (s, e) => MessageBox.Show("Flushing DNS cache...");
+            // btnDNS
+            this.btnDNS.Location = new System.Drawing.Point(60, 160);
+            this.btnDNS.Size = new System.Drawing.Size(250, 45);
+            this.btnDNS.Text = "مسح DNS Cache";
+            this.btnDNS.Click += new System.EventHandler(this.btnDNS_Click);
 
-            btnAll.Location = new Point(50, 230);
-            btnAll.Size = new Size(250, 45);
-            btnAll.Text = "Clean All";
-            btnAll.Click += (s, e) => MessageBox.Show("Cleaning all system caches...");
+            // btnAll
+            this.btnAll.Location = new System.Drawing.Point(60, 220);
+            this.btnAll.Size = new System.Drawing.Size(250, 45);
+            this.btnAll.Text = "تنظيف الكل";
+            this.btnAll.Click += new System.EventHandler(this.btnAll_Click);
 
-            Controls.AddRange(new Control[] { btnTemp, btnCache, btnDNS, btnAll });
-            Size = new Size(700, 500);
-            ResumeLayout(false);
+            // UC_Cleaning
+            this.Controls.Add(this.btnCleanTemp);
+            this.Controls.Add(this.btnCleanCache);
+            this.Controls.Add(this.btnDNS);
+            this.Controls.Add(this.btnAll);
+            this.Name = "UC_Cleaning";
+            this.Size = new System.Drawing.Size(400, 320);
+            this.ResumeLayout(false);
         }
+        #endregion
     }
 }
