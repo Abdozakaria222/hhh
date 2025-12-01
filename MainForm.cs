32ba11389fb605b1018dc065361ab65e450a0b2a
Abdozakaria222 <abdozakaria222@gmail.com>
2025-12-01 02:11:16 +0200

    Refactor MainForm to new namespace and remove controls
    Removed functionality for loading controls and buttons.

--- MainForm.cs
+++ MainForm.cs
@@ -1,36 +1,13 @@
 using System;
 using System.Windows.Forms;
 
-namespace SystemOptimizer
+namespace hhh
 {
     public partial class MainForm : Form
     {
         public MainForm()
         {
             InitializeComponent();
-        }
-
-        // تغيير المحتوى داخل panelMain
-        private void LoadControl(Control ctrl)
-        {
-            panelMain.Controls.Clear();
-            ctrl.Dock = DockStyle.Fill;
-            panelMain.Controls.Add(ctrl);
-        }
-
-        private void btnServices_Click(object sender, EventArgs e)
-        {
-            LoadControl(new UC_Services());
-        }
-
-        private void btnCleaning_Click(object sender, EventArgs e)
-        {
-            LoadControl(new UC_Cleaning());
-        }
-
-        private void btnTweaks_Click(object sender, EventArgs e)
-        {
-            LoadControl(new UC_Tweaks());
         }
     }
 }
