0a6e06f232ded2e856b89715088a0ca265c238a3
Abdozakaria222 <abdozakaria222@gmail.com>
2025-12-01 02:54:35 +0200

    Update UC_Services.Designer.cs

--- UC_Services.Designer.cs
+++ UC_Services.Designer.cs
@@ -3,30 +3,61 @@ namespace hhh
     partial class UC_Services
     {
         private System.ComponentModel.IContainer components = null;
-        private Button btnStop;
-        private Button btnRestore;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnRestore;
 
+        /// <summary> 
+        /// تنظيف الموارد المُستخدمة.
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region كود التهيئة
         private void InitializeComponent()
         {
-            this.btnStop = new Button();
-            this.btnRestore = new Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnRestore = new System.Windows.Forms.Button();
             this.SuspendLayout();
 
+            // 
             // btnStop
-            this.btnStop.Text = "إيقاف الخدمات";
-            this.btnStop.Location = new System.Drawing.Point(50, 50);
+            // 
+            this.btnStop.Location = new System.Drawing.Point(60, 40);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(250, 45);
+            this.btnStop.TabIndex = 0;
+            this.btnStop.Text = "إيقاف الخدمات غير الضرورية";
+            this.btnStop.UseVisualStyleBackColor = true;
             this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
 
+            // 
             // btnRestore
-            this.btnRestore.Text = "استعادة الإعدادات";
-            this.btnRestore.Location = new System.Drawing.Point(50, 110);
+            // 
+            this.btnRestore.Location = new System.Drawing.Point(60, 100);
+            this.btnRestore.Name = "btnRestore";
+            this.btnRestore.Size = new System.Drawing.Size(250, 45);
+            this.btnRestore.TabIndex = 1;
+            this.btnRestore.Text = "استعادة الإعدادات الافتراضية";
+            this.btnRestore.UseVisualStyleBackColor = true;
             this.btnRestore.Click += new System.EventHandler(this.btnRestore_Click);
 
+            // 
             // UC_Services
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.btnStop);
             this.Controls.Add(this.btnRestore);
-            this.Size = new System.Drawing.Size(400, 300);
+            this.Name = "UC_Services";
+            this.Size = new System.Drawing.Size(400, 200);
             this.ResumeLayout(false);
         }
+        #endregion
     }
 }
