84aebc56a742a0705e3dfdaff7b615a8a587660d
Abdozakaria222 <abdozakaria222@gmail.com>
2025-12-01 02:12:36 +0200

    Refactor cleaning buttons and update event handlers

--- UC_Cleaning.Designer.cs
+++ UC_Cleaning.Designer.cs
@@ -6,9 +6,8 @@ namespace hhh
 {
     public partial class UC_Cleaning : UserControl
     {
-        private System.ComponentModel.IContainer components = null;
-        private Button btnCleanTemp;
-        private Button btnCleanCache;
+        private Button btnTemp;
+        private Button btnCache;
         private Button btnDNS;
         private Button btnAll;
 
@@ -17,53 +16,38 @@ namespace hhh
             InitializeComponent();
         }
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
         private void InitializeComponent()
         {
-            this.btnCleanTemp = new Button();
-            this.btnCleanCache = new Button();
+            this.btnTemp = new Button();
+            this.btnCache = new Button();
             this.btnDNS = new Button();
             this.btnAll = new Button();
             this.SuspendLayout();
 
-            // btnCleanTemp
-            this.btnCleanTemp.Location = new Point(30, 40);
-            this.btnCleanTemp.Size = new Size(260, 45);
-            this.btnCleanTemp.Text = "Clean TEMP";
-            this.btnCleanTemp.Click += new EventHandler(this.btnCleanTemp_Click);
+            this.btnTemp.Location = new Point(50, 50);
+            this.btnTemp.Size = new Size(250, 45);
+            this.btnTemp.Text = "Clean TEMP";
+            this.btnTemp.Click += (s, e) => MessageBox.Show("Cleaning TEMP files...");
 
-            // btnCleanCache
-            this.btnCleanCache.Location = new Point(30, 100);
-            this.btnCleanCache.Size = new Size(260, 45);
-            this.btnCleanCache.Text = "Clean Cache";
-            this.btnCleanCache.Click += new EventHandler(this.btnCleanCache_Click);
+            this.btnCache.Location = new Point(50, 110);
+            this.btnCache.Size = new Size(250, 45);
+            this.btnCache.Text = "Clean Cache";
+            this.btnCache.Click += (s, e) => MessageBox.Show("Cleaning Cache...");
 
-            // btnDNS
-            this.btnDNS.Location = new Point(30, 160);
-            this.btnDNS.Size = new Size(260, 45);
+            this.btnDNS.Location = new Point(50, 170);
+            this.btnDNS.Size = new Size(250, 45);
             this.btnDNS.Text = "Flush DNS";
-            this.btnDNS.Click += new EventHandler(this.btnDNS_Click);
+            this.btnDNS.Click += (s, e) => MessageBox.Show("Flushing DNS cache...");
 
-            // btnAll
-            this.btnAll.Location = new Point(30, 220);
-            this.btnAll.Size = new Size(260, 45);
+            this.btnAll.Location = new Point(50, 230);
+            this.btnAll.Size = new Size(250, 45);
             this.btnAll.Text = "Clean All";
-            this.btnAll.Click += new EventHandler(this.btnAll_Click);
+            this.btnAll.Click += (s, e) => MessageBox.Show("Cleaning all system caches...");
 
-            // UC_Cleaning
-            this.Controls.Add(this.btnCleanTemp);
-            this.Controls.Add(this.btnCleanCache);
+            this.Controls.Add(this.btnTemp);
+            this.Controls.Add(this.btnCache);
             this.Controls.Add(this.btnDNS);
             this.Controls.Add(this.btnAll);
-            this.Name = "UC_Cleaning";
             this.Size = new Size(700, 500);
             this.ResumeLayout(false);
         }
