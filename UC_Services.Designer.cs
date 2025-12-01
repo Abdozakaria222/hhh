a2d795cc8d2fd6bcf6bb7299b351b18c9b35c51a
Abdozakaria222 <abdozakaria222@gmail.com>
2025-12-01 02:12:18 +0200

    Refactor button layout in UC_Services
    Updated button positions and sizes in UC_Services.

--- UC_Services.Designer.cs
+++ UC_Services.Designer.cs
@@ -6,7 +6,6 @@ namespace hhh
 {
     public partial class UC_Services : UserControl
     {
-        private System.ComponentModel.IContainer components = null;
         private Button btnStop;
         private Button btnRestore;
 
@@ -15,39 +14,36 @@ namespace hhh
             InitializeComponent();
         }
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
         private void InitializeComponent()
         {
             this.btnStop = new Button();
             this.btnRestore = new Button();
             this.SuspendLayout();
 
-            // btnStop
-            this.btnStop.Location = new Point(30, 40);
-            this.btnStop.Size = new Size(260, 45);
+            this.btnStop.Location = new Point(50, 60);
+            this.btnStop.Size = new Size(250, 45);
             this.btnStop.Text = "Stop Unneeded Services";
             this.btnStop.Click += new EventHandler(this.btnStop_Click);
 
-            // btnRestore
-            this.btnRestore.Location = new Point(30, 100);
-            this.btnRestore.Size = new Size(260, 45);
+            this.btnRestore.Location = new Point(50, 130);
+            this.btnRestore.Size = new Size(250, 45);
             this.btnRestore.Text = "Restore Default Services";
             this.btnRestore.Click += new EventHandler(this.btnRestore_Click);
 
-            // UC_Services
             this.Controls.Add(this.btnStop);
             this.Controls.Add(this.btnRestore);
-            this.Name = "UC_Services";
             this.Size = new Size(700, 500);
             this.ResumeLayout(false);
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Stopping unnecessary services...");
+        }
+
+        private void btnRestore_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Restoring default Windows services...");
         }
     }
 }
