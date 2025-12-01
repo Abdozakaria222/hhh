907071e9b737c384538c5e6c8cba33425ae0ec63
Abdozakaria222 <abdozakaria222@gmail.com>
2025-12-01 02:24:18 +0200

    Refactor UC_Services to include Dispose method
    Added Dispose method to manage component resources.

--- UC_Services.Designer.cs
+++ UC_Services.Designer.cs
@@ -6,34 +6,38 @@ namespace hhh
 {
     public partial class UC_Services : UserControl
     {
+        private System.ComponentModel.IContainer components = null;
         private Button btnStop;
         private Button btnRestore;
 
-        public UC_Services()
+        public UC_Services() { InitializeComponent(); }
+
+        protected override void Dispose(bool disposing)
         {
-            InitializeComponent();
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
         }
 
         private void InitializeComponent()
         {
-            this.btnStop = new Button();
-            this.btnRestore = new Button();
-            this.SuspendLayout();
+            btnStop = new Button();
+            btnRestore = new Button();
+            SuspendLayout();
 
-            this.btnStop.Location = new Point(50, 60);
-            this.btnStop.Size = new Size(250, 45);
-            this.btnStop.Text = "Stop Unneeded Services";
-            this.btnStop.Click += new EventHandler(this.btnStop_Click);
+            btnStop.Location = new Point(50, 60);
+            btnStop.Size = new Size(250, 45);
+            btnStop.Text = "Stop Unneeded Services";
+            btnStop.Click += btnStop_Click;
 
-            this.btnRestore.Location = new Point(50, 130);
-            this.btnRestore.Size = new Size(250, 45);
-            this.btnRestore.Text = "Restore Default Services";
-            this.btnRestore.Click += new EventHandler(this.btnRestore_Click);
+            btnRestore.Location = new Point(50, 130);
+            btnRestore.Size = new Size(250, 45);
+            btnRestore.Text = "Restore Default Services";
+            btnRestore.Click += btnRestore_Click;
 
-            this.Controls.Add(this.btnStop);
-            this.Controls.Add(this.btnRestore);
-            this.Size = new Size(700, 500);
-            this.ResumeLayout(false);
+            Controls.Add(btnStop);
+            Controls.Add(btnRestore);
+            Size = new Size(700, 500);
+            ResumeLayout(false);
         }
 
         private void btnStop_Click(object sender, EventArgs e)
