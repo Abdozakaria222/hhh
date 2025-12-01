75d5cbd196fefe954744ed73ae3d829ce7fea84e
Abdozakaria222 <abdozakaria222@gmail.com>
2025-12-01 02:41:25 +0200

    Update UC_Tweaks.cs

--- UC_Tweaks.cs
+++ UC_Tweaks.cs
@@ -3,7 +3,7 @@ using System.Diagnostics;
 using System.Windows.Forms;
 using Microsoft.Win32;
 
-namespace SystemOptimizer
+namespace hhh
 {
     public partial class UC_Tweaks : UserControl
     {
@@ -16,32 +16,42 @@ namespace SystemOptimizer
         {
             try
             {
-                RegistryKey k1 = Registry.LocalMachine.CreateSubKey(@"SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management");
-                k1.SetValue("ClearPageFileAtShutdown", 1, RegistryValueKind.DWord);
+                using (var mm = Registry.LocalMachine.CreateSubKey(
+                    @"SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management"))
+                {
+                    mm?.SetValue("ClearPageFileAtShutdown", 1, RegistryValueKind.DWord);
+                }
 
-                RegistryKey k2 = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile");
-                k2.SetValue("NetworkThrottlingIndex", 0xffffffff, RegistryValueKind.DWord);
+                using (var net = Registry.LocalMachine.CreateSubKey(
+                    @"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile"))
+                {
+                    net?.SetValue("NetworkThrottlingIndex", 0xffffffff, RegistryValueKind.DWord);
+                }
 
-                MessageBox.Show("تم تطبيق تحسينات الريجستري.");
+                MessageBox.Show("تم تطبيق تحسينات الريجستري.", "Tweaks");
             }
             catch
             {
-                MessageBox.Show("قم بتشغيل البرنامج كمسؤول.");
+                MessageBox.Show("شغّل البرنامج كمسؤول لتطبيق هذه التعديلات.", "Tweaks");
             }
         }
 
         private void btnFreeRAM_Click(object sender, EventArgs e)
         {
-            Process.Start("cmd.exe", "/c echo FreeMem=Space(2621440000) > %temp%\\ram.vbs");
-            Process.Start("cmd.exe", "/c cscript %temp%\\ram.vbs");
-            MessageBox.Show("تم تفريغ الرام.");
+            try
+            {
+                Process.Start("cmd.exe", "/c echo FreeMem=Space(2621440000) > %temp%\\ram.vbs");
+                Process.Start("cmd.exe", "/c cscript //nologo %temp%\\ram.vbs");
+            }
+            catch { }
+            MessageBox.Show("تم تفريغ جزء من الذاكرة.", "Tweaks");
         }
 
         private void btnOptimizeAll_Click(object sender, EventArgs e)
         {
             btnRegistryTweaks_Click(sender, e);
             btnFreeRAM_Click(sender, e);
-            MessageBox.Show("تم التحسين الكامل!");
+            MessageBox.Show("تم تنفيذ جميع تحسينات الأداء.", "Tweaks");
         }
     }
 }
