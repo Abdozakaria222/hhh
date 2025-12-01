1b4d688726371a68867815f1701443624a3b605f
Abdozakaria222 <abdozakaria222@gmail.com>
2025-12-01 02:46:56 +0200

    Update UC_Services.Designer.cs

--- UC_Services.Designer.cs
+++ UC_Services.Designer.cs
@@ -1,53 +1,32 @@
-using System;
-using System.Drawing;
-using System.Windows.Forms;
-
 namespace hhh
 {
-    public partial class UC_Services : UserControl
+    partial class UC_Services
     {
         private System.ComponentModel.IContainer components = null;
         private Button btnStop;
         private Button btnRestore;
 
-        public UC_Services() { InitializeComponent(); }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null)) components.Dispose();
-            base.Dispose(disposing);
-        }
-
         private void InitializeComponent()
         {
-            btnStop = new Button();
-            btnRestore = new Button();
-            SuspendLayout();
+            this.btnStop = new Button();
+            this.btnRestore = new Button();
+            this.SuspendLayout();
 
-            btnStop.Location = new Point(50, 60);
-            btnStop.Size = new Size(250, 45);
-            btnStop.Text = "Stop Unneeded Services";
-            btnStop.Click += btnStop_Click;
+            // btnStop
+            this.btnStop.Text = "إيقاف الخدمات";
+            this.btnStop.Location = new System.Drawing.Point(50, 50);
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
 
-            btnRestore.Location = new Point(50, 130);
-            btnRestore.Size = new Size(250, 45);
-            btnRestore.Text = "Restore Default Services";
-            btnRestore.Click += btnRestore_Click;
+            // btnRestore
+            this.btnRestore.Text = "استعادة الإعدادات";
+            this.btnRestore.Location = new System.Drawing.Point(50, 110);
+            this.btnRestore.Click += new System.EventHandler(this.btnRestore_Click);
 
-            Controls.Add(btnStop);
-            Controls.Add(btnRestore);
-            Size = new Size(700, 500);
-            ResumeLayout(false);
-        }
-
-        private void btnStop_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Stopping unnecessary services...");
-        }
-
-        private void btnRestore_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Restoring default Windows services...");
+            // UC_Services
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnRestore);
+            this.Size = new System.Drawing.Size(400, 300);
+            this.ResumeLayout(false);
         }
     }
 }
